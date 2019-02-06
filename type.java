b03750d78bb73463911f94ad2cab482dbd221961
Ankur Vadhwani <www.nankur113@gmail.com>
2019-02-06 19:53:02 +0530

    Update type.java

--- type.java
+++ type.java
@@ -1,12 +1,12 @@
 // storing the input taken from cmdline in variable
 
 import java.lang.*;
-imprt java.io.*;
+import java.io.*;
 
 class type{
   public static void main(String args[])
   {
-    int a=Int.parseInt(args[0]);
+    int a=int.parseInt(args[0]);
     float f = Float.parseFloat(args[1]);
     double d = Double.parseDouble(args[2]);
     
